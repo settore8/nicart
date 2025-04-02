0ce610f00ed17a00fe1a308c45a7f33194d9e75c
settore8 <effe@settore8.it>
2025-04-02 10:42:01 +0200

    pubblicazione posttype faq

--- functions/_posttypes.php
+++ functions/_posttypes.php
@@ -93,6 +93,46 @@ add_action( 'template_redirect', function() {
 });
 
 
+add_action( 'init', 'codex_faq_init' );
+
+function codex_faq_init() {
+	$labels = array(
+		'name'               => _x( 'Faqs', 'post type general name', 'your-plugin-textdomain' ),
+		'singular_name'      => _x( 'Faq', 'post type singular name', 'your-plugin-textdomain' ),
+		'menu_name'          => _x( 'Faqs', 'admin menu', 'your-plugin-textdomain' ),
+		'name_admin_bar'     => _x( 'faq', 'add new on admin bar', 'your-plugin-textdomain' ),
+		'add_new'            => _x( 'Aggiungi faq', 'privati', 'your-plugin-textdomain' ),
+		'add_new_item'       => __( 'Aggiungi nuovo faq', 'your-plugin-textdomain' ),
+		'new_item'           => __( 'Nuovo faq', 'your-plugin-textdomain' ),
+		'edit_item'          => __( 'Modifico faq', 'your-plugin-textdomain' ),
+		'view_item'          => __( 'Vedi i faq', 'your-plugin-textdomain' ),
+		'search_items'       => __( 'Cerca faq', 'your-plugin-textdomain' ),
+		'parent_item_colon'  => __( 'Parent faq:', 'your-plugin-textdomain' ),
+		'not_found'          => __( 'Nessuna faq trovato.', 'your-plugin-textdomain' ),
+		'not_found_in_trash' => __( 'Nessuna faq trovato nel cestino.', 'your-plugin-textdomain' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+    	'description'        => __( 'Description.', 'your-plugin-textdomain' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite' => array(
+	    'slug' => 'faq',
+	    	'with_front' => true
+	  	),
+		'has_archive'        => 'domande-frequenti',
+		'capability_type'    => 'post',
+		'hierarchical'       => false,
+		'supports'           => array( 'title', 'page-attributes' ),
+		);
+	register_post_type( 'faq', $args );
+}
+
+
 
 
 ?>

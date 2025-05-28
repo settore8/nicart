87304253568702b1219612aed2a52a9235b546d4
settore8 <effe@settore8.it>
2025-05-28 17:41:22 +0200

    fix grid

--- archive-video.php
+++ archive-video.php
@@ -16,14 +16,14 @@
 				<section class="col-xs-12">
 					<?php
 					if ( have_posts() ) { ?>
-						<div class="row">
+						<div class="video-grid">
 							
 						<?php while ( have_posts() ) {
 							the_post();
 							$video = get_field('video_youtube');
 							$poster = get_field('poster');
 							if( $video && $poster): ?>
-							<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+							<div class="">
 								<a class="video-preview" href="<?php echo get_permalink(); ?>" target="_blank">
 									<?php 
 									if($poster) : ?>
